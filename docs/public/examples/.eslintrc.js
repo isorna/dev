256c838e2e6711044df587990d937d008d1d0ef0
Alvaro Isorna <alvaro.isorna@gmail.com>
2023-07-18 21:04:13 +0200

    docs(articles): :memo: ideas for articles

---
+++ docs/public/examples/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      './docs/tsconfig.json'
+    ]
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
